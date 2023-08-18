f829088c603457ec2a15913ecc39b742f1543c1f
Sohail Ahmed <sohail.bscsf19@iba-suk.edu.pk>
2023-08-18 23:52:53 +0500

    Happy learning with ReactJs

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
+import { Component } from 'react';
 import './App.css';
 
+
+function MyComponent({nameForClass, name, income=10}) {
+  return( 
+  <>
+  <div className={nameForClass}>
+    {name}: ${income}</div>
+    <hr/>
+    </>
+  );
+}
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <div>Hello <MyComponent name={"Asad"} nameForClass="div1" income={3000}/> 
+  <MyComponent name={"Atif"}nameForClass="div2"  /> 
+  <MyComponent name={"Arif"}nameForClass="div3" income={4000}/> 
+   Guys</div>
   );
 }
 
