c860cd8291becf21a8bfe6b60c73f5061bab2866
Sohail Ahmed <sohail.bscsf19@iba-suk.edu.pk>
2023-08-19 11:51:57 +0500

    Components and styling in ReactJs

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import { Component } from 'react';
-import './App.css';
+import "./App.css";
+import Header from "./Components/Header";
 
-
-function MyComponent({nameForClass, name, income=10}) {
-  return( 
-  <>
-  <div className={nameForClass}>
-    {name}: ${income}</div>
-    <hr/>
-    </>
+function App(){
+ return(
+  <div>
+Hello 
+<Header />
+Guys
+  </div>
   );
 }
-function App() {
-  return (
-  <div>Hello <MyComponent name={"Asad"} nameForClass="div1" income={3000}/> 
-  <MyComponent name={"Atif"}nameForClass="div2"  /> 
-  <MyComponent name={"Arif"}nameForClass="div3" income={4000}/> 
-   Guys</div>
-  );
-}
-
 export default App;
---
+++ src/Components/Header.js
@@ -0,0 +1,15 @@
+import React from "react"; 
+import "./Header.css"
+
+const Header = () => {
+const headerStyle = {
+  border: "2px solid red",
+  padding: "2mvx",
+  backgroundColor:"red",
+};
+
+  return <nav
+  style={headerStyle}>Header</nav>
+}
+
+export default Header
